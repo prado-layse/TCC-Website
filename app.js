e363e22575d5c9c46ac1682c1f446e0eed46e7b7
Layse Prado <laysesprado@gmail.com>
2024-10-14 20:58:56 -0300

    Conexão com o banco de dados 'hoqueibrasil'

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const express = require("express");
 
 const app = express();
 
+//Conexao BD (Será alterado depois o local)
+const mysql = require('mysql');
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: '123456',
+    database: 'hoqueibrasil'
+});
+
+//Testando a conexao
+conexao.connect(function(err){
+    if(err){
+        console.error('Erro Conexão: ' + err.stack);
+        return;
+    } 
+
+    console.log('Conexão com o ID: ' + conexao.threadId);
+})
+
 app.get("/", function(require, response){
     response.sendFile(__dirname + "/src/index.html");
 });
