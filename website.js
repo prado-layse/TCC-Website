14f3f361117ab285f962f2d5663839e087679ada
Layse Prado <laysesprado@gmail.com>
2024-10-18 20:52:01 -0300

    CRUD: Listagem de Federações

--- website.js
+++ website.js
@@ -2,21 +2,38 @@ const express = require("express");
 const site = express();
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-const federacao = require("./models/Federacao");
+const moment = require("moment");
+const Federacao = require("./models/Federacao");
 
 // Configure o motor de visualização
-site.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+site.engine('handlebars', handlebars.engine({ 
+    defaultLayout: 'main',
+    helpers: {
+        formatDate: (date) => {
+            return moment(date).format('DD/MM/YYYY')
+        }
+    }
+    //Exemplo de como usar o formaDate no arquivo de handlebars
+    //Campo: {{#formatDate campo-bd}}{{/formatDate}}<br>
+}));
 site.set('view engine', 'handlebars');
 site.use(bodyParser.urlencoded({ extended: false }));
 site.use(bodyParser.json());
 
 // Rotas
+site.get('/federacao', function(req, res){
+    Federacao.findAll().then(function(federacoes){
+        const federacoesData = federacoes.map(f => f.dataValues)
+        res.render('pag-federacoes', {federacoes: federacoesData});
+    })
+})
+
 site.get('/cadastro', function(req, res){
     res.render('cadastro-federacao');
 });
 
 site.post('/add-federacao', function(req, res){
-    federacao.create({
+    Federacao.create({
         razaoSocial: req.body.razaoSocial,
         nomeFantasia: req.body.nomeFantasia,
         sigla: req.body.sigla,
@@ -31,5 +48,5 @@ site.post('/add-federacao', function(req, res){
 
 // Inicie o servidor
 site.listen(8080, () => {
-    console.log('Servidor rodando na porta 8080');
+    console.log('Servidor rodando na porta 8080: http://localhost:8080/');
 });
