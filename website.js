1215e526aa55ab587709b961a6ae81e862de7a03
Layse Prado <laysesprado@gmail.com>
2024-10-19 10:41:40 -0300

    API-Middleware

--- website.js
+++ website.js
@@ -1,24 +1,33 @@
-const express = require("express");
+const express = require('express');
+const moment = require('moment');
+const { engine } = require('express-handlebars'); // Importar a função 'engine' corretamente
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Federacao = require('./models/Federacao');
+
 const site = express();
-const handlebars = require("express-handlebars");
-const bodyParser = require("body-parser");
-const moment = require("moment");
-const Federacao = require("./models/Federacao");
 
 // Configure o motor de visualização
-site.engine('handlebars', handlebars.engine({ 
+site.engine('handlebars', engine({
     defaultLayout: 'main',
     helpers: {
         formatDate: (date) => {
             return moment(date).format('DD/MM/YYYY');
         }
     }
-    //Exemplo de como usar o formaDate no arquivo de handlebars
-    //Campo: {{#formatDate campo-bd}}{{/formatDate}}<br>
 }));
+
 site.set('view engine', 'handlebars');
 site.use(bodyParser.urlencoded({ extended: false }));
 site.use(bodyParser.json());
+site.use(cors());
+
+// Middleware para log
+site.use((req, res, next) => {
+    console.log("Acessou o Middleware!");
+    site.use(cors());
+    next();
+});
 
 // ROTA: Tela Inicial
 site.get('/', (req, res) => {
@@ -30,10 +39,10 @@ site.get('/federacoes', (req, res) => {
     console.log('Requisição recebida para listar federações');
     Federacao.findAll()
         .then(federacoes => {
-            res.status(200).json(federacoes); // Retorna as federações com status 200
+            res.status(200).json(federacoes);
         })
         .catch(erro => {
-            console.error(erro); // Log do erro para o console
+            console.error(erro);
             res.status(500).json({ error: "Erro ao listar federações", message: erro.message });
         });
 });
@@ -65,7 +74,7 @@ site.get('/cadastro', (req, res) => {
     res.render('cadastro-federacao');
 });
 
-//Adicionar uma Nova Federação
+// Adicionar uma Nova Federação
 site.post('/add-federacao', (req, res) => {
     Federacao.create({
         razaoSocial: req.body.razaoSocial,
@@ -75,10 +84,10 @@ site.post('/add-federacao', (req, res) => {
         presidente: req.body.presidente
     })
     .then(novaFederacao => {
-        res.status(201).json(novaFederacao); // Retorna a nova federação criada com status 201
+        res.status(201).json(novaFederacao);
     })
     .catch(erro => {
-        console.error(erro); // Log do erro para o console
+        console.error(erro);
         res.status(500).json({ error: "Erro ao adicionar federação", message: erro.message });
     });
 });
@@ -88,7 +97,7 @@ site.get('/editar-federacao/:codFederacao', (req, res) => {
     Federacao.findOne({ where: { codFederacao: req.params.codFederacao } })
         .then(federacao => {
             if (federacao) {
-                res.render('editar-federacao', { federacao: federacao.dataValues }); // Passa os dados para a página
+                res.render('editar-federacao', { federacao: federacao.dataValues });
             } else {
                 res.status(404).send("Federação não encontrada");
             }
@@ -111,13 +120,14 @@ site.post('/atualizar-federacao/:codFederacao', (req, res) => {
         where: { codFederacao: req.params.codFederacao }
     })
     .then(() => {
-        res.redirect('/federacoes'); // Redireciona para a lista de federações após a atualização
+        res.redirect('/federacoes');
     })
     .catch(erro => {
         console.error(erro);
         res.status(500).send("Erro ao atualizar a federação");
     });
 });
+
 // Inicie o servidor
 site.listen(3000, () => {
     console.log('Servidor rodando na porta 3000: http://localhost:3000/');
