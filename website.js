bd1c4182664c4c5cd0772461f15b3c6de8312482
Layse Prado <laysesprado@gmail.com>
2024-10-18 22:19:54 -0300

    CRUD: Cadastrar Nova Federação via API

--- website.js
+++ website.js
@@ -10,7 +10,7 @@ site.engine('handlebars', handlebars.engine({
     defaultLayout: 'main',
     helpers: {
         formatDate: (date) => {
-            return moment(date).format('DD/MM/YYYY')
+            return moment(date).format('DD/MM/YYYY');
         }
     }
     //Exemplo de como usar o formaDate no arquivo de handlebars
@@ -20,33 +20,55 @@ site.set('view engine', 'handlebars');
 site.use(bodyParser.urlencoded({ extended: false }));
 site.use(bodyParser.json());
 
-// Rotas
-site.get('/federacao', function(req, res){
-    Federacao.findAll().then(function(federacoes){
-        const federacoesData = federacoes.map(f => f.dataValues)
-        res.render('pag-federacoes', {federacoes: federacoesData});
-    })
-})
+// ROTA: Tela Inicial
+site.get('/', (req, res) => {
+    res.send("TELA INICIAL");
+});
 
-site.get('/cadastro', function(req, res){
+// ROTA: Listar Federações
+site.get('/federacao', (req, res) => {
+    console.log('Requisição recebida para listar federações');
+    Federacao.findAll()
+        .then(federacoes => {
+            res.status(200).json(federacoes); // Retorna as federações com status 200
+        })
+        .catch(erro => {
+            console.error(erro); // Log do erro para o console
+            res.status(500).json({ error: "Erro ao listar federações", message: erro.message });
+        });
+});
+
+// ROTA: Tela de Cadastro de Federação
+site.get('/cadastro', (req, res) => {
     res.render('cadastro-federacao');
 });
 
-site.post('/add-federacao', function(req, res){
+// ROTA: Adicionar uma Nova Federação
+site.post('/add-federacao', (req, res) => {
     Federacao.create({
         razaoSocial: req.body.razaoSocial,
         nomeFantasia: req.body.nomeFantasia,
         sigla: req.body.sigla,
         cnpj: req.body.cnpj,
         presidente: req.body.presidente
-    }).then(function(){
-        res.send("Federação cadsastrada com sucesso")
-    }).catch(function(erro){
-        res.send("Erro: " + erro.message);
-    });    
+    })
+    .then(novaFederacao => {
+        res.status(201).json(novaFederacao); // Retorna a nova federação criada com status 201
+    })
+    .catch(erro => {
+        console.error(erro); // Log do erro para o console
+        res.status(500).json({ error: "Erro ao adicionar federação", message: erro.message });
+    });
 });
 
 // Inicie o servidor
-site.listen(8080, () => {
-    console.log('Servidor rodando na porta 8080: http://localhost:8080/');
+site.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000: http://localhost:3000/');
 });
+
+// Rotas
+/*site.get('/federacao', function(req, res){
+    Federacao.findAll().then(function(federacoes){
+        res.render('pag-federacoes', {federacoes});
+    })
+})*/
