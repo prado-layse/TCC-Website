6da6f4ca6591e85d72738dca21410109ecc03eca
WalaceSilva03 <walace.jose00@gmail.com>
2025-02-03 23:33:57 -0300

    Implementação de envio JSON, ainda incompleto e com necessidade de testes.

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -29,19 +29,28 @@ exports.login = async (req, res) => {
             }
 
             // Redirecionamento baseado no perfil do usuário
-            if (usuario.perfil === 'user-admin') {
-                return res.redirect('/api/admin/cbhp');
-            } else if (usuario.perfil === 'user-clube') {
-                const clube = await Clube.findOne({ where: { codUsuario: usuario.codUsuario }});
-                if (!clube) {
-                    throw { status: 404, message: 'Clube não encontrado.' };
-                }
-
-                req.session.usuario.codUsuario = clube.codUsuario;
-                req.session.usuario.sigla = clube.sigla;
-                return res.redirect(`/api/clubes/dashboard/${clube.sigla}`);
+            if (req.headers.accept && req.headers.accept.includes('application/json')) {
+                // Retorna um JSON com os dados do usuário ou alguma mensagem
+                return res.status(200).json({ 
+                    message: 'Login realizado com sucesso', 
+                    usuario 
+                });
             } else {
-                return res.redirect('/api/cbhp');
+                // Redireciona com base no perfil do usuário
+                if (usuario.perfil === 'user-admin') {
+                    return res.redirect('/api/admin/cbhp');
+                } else if (usuario.perfil === 'user-clube') {
+                    const clube = await Clube.findOne({ where: { codUsuario: usuario.codUsuario }});
+                    if (!clube) {
+                        throw { status: 404, message: 'Clube não encontrado.' };
+                    }
+
+                    req.session.usuario.codUsuario = clube.codUsuario;
+                    req.session.usuario.sigla = clube.sigla;
+                    return res.redirect(`/api/clubes/dashboard/${clube.sigla}`);
+                } else {
+                    return res.redirect('/api/cbhp');
+                }
             }
         });
 
@@ -50,64 +59,6 @@ exports.login = async (req, res) => {
         res.status(error.status || 500).json({ message: error.message || 'Erro ao fazer login.' });
     }
 };
-    /*
-    exports.login = async (req, res) => {
-    const { email, senha } = req.body;
-
-    try {
-        // Busca o usuário pelo email
-        const usuario = await Usuario.findOne({ where: { email } });
-
-        if (!usuario) {
-            return res.status(401).json({ message: 'Email ou senha incorretos.' });
-        }
-
-        // Verifica a senha
-        const isPasswordValid = await bcrypt.compare(senha, usuario.senha);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Email ou senha incorretos.' });
-        }
-
-        // Salvar os dados do usuário na sessão
-        req.session.userId = usuario.codUsuario; // Salvar userId na sessão
-        req.session.usuario = {
-            codUsuario: usuario.codUsuario,
-            perfil: usuario.perfil,
-            email: usuario.email,
-            isAdmin: usuario.perfil === 'user-admin'
-        };
-
-        req.session.save(async (err) => {
-            if (err) {
-                console.error('Erro ao salvar a sessão:', err);
-                return res.status(500).json({ message: 'Erro ao iniciar sessão.' });
-            }
-
-            // Redirecionar baseado no perfil
-            if (usuario.perfil === 'user-admin') {
-                // Redirecionar para o dashboard do admin
-                return res.redirect('/api/admin/cbhp');
-            } else if (usuario.perfil === 'user-clube') {
-                // Busca o clube associado ao usuário
-                const clube = await Clube.findOne({ where: { codUsuario: usuario.codUsuario } });
-
-                if (clube) {
-                    // Redirecionar para o dashboard do clube
-                    req.session.usuario.codClube = clube.codClube;
-                    req.session.usuario.sigla = clube.sigla;
-                    return res.redirect(`/api/clubes/dashboard/${clube.sigla}`);
-                } else {
-                    return res.status(404).json({ message: 'Clube não encontrado.' });
-                }
-            } else if (usuario.perfil === 'user-consulta') {
-                try {
-                    return res.redirect('/api/cbhp');
-                } catch (error) {
-                    console.error('Erro no redirecionamento.', error);
-                    return res.status(404).json({ message: 'Erro no redirecionamento.' });
-                }
-            }
-    */
 
 // Adicionar Novo Usuario
 exports.adicionarUsuario = async (req, res) => {
